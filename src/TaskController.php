2dd3efd05714c143a647d95f1f144c1f47b2babb
rod773 <104315743+rod773@users.noreply.github.com>
2023-06-29 10:56:03 +0200

    validation errors message

--- src/TaskController.php
+++ src/TaskController.php
@@ -13,16 +13,22 @@ class TaskController
         if ($id == null) {
 
             if ($method == "GET") {
-                
+
                 echo json_encode($this->gateway->getAll());
             } else if ($method == "POST") {
-                
-                $data = (array)json_decode(file_get_contents('php://input'),true);
+
+                $data = (array)json_decode(file_get_contents('php://input'), true);
+
+                $errors = $this->getValidationErrors($data);
+
+                if (!empty($errors)) {
+                    $this->respondUnprocessableEntity($errors);
+                    return;
+                }
 
                 $id = $this->gateway->create($data);
 
                 $this->respondCreated($id);
-
             } else {
                 $this->respondMethodNotAllowed("GET,POST");
             }
@@ -30,7 +36,7 @@ class TaskController
 
             $task = $this->gateway->get($id);
 
-            if($task===false){
+            if ($task === false) {
                 $this->respondNotFound($id);
                 return;
             }
@@ -38,7 +44,7 @@ class TaskController
             switch ($method) {
 
                 case "GET":
-                    echo 
+                    echo
                     json_encode($task);
                     break;
 
@@ -56,7 +62,13 @@ class TaskController
         }
     }
 
-
+    private function respondUnprocessableEntity($errors)
+    {
+        http_response_code(422);
+        echo json_encode([
+            "errors" => $errors
+        ]);
+    }
 
     private function respondMethodNotAllowed($allowed_methods)
     {
@@ -65,19 +77,42 @@ class TaskController
     }
 
 
-    private function respondNotFound($id){
+    private function respondNotFound($id)
+    {
         http_response_code((404));
 
         echo json_encode([
-            "message"=>"task with id $id not found"
+            "message" => "task with id $id not found"
         ]);
     }
 
-    private function respondCreated($id){
-        
+    private function respondCreated($id)
+    {
+
         http_response_code(201);
         echo json_encode([
-            "message"=>"task created, id => $id"
+            "message" => "task created, id => $id"
         ]);
+    }
+
+    private function getValidationErrors($data)
+    {
+
+        $errors = [];
+
+        if (empty($data['name'])) {
+
+            $errors[] = "name is required";
+        }
+
+
+        if (!empty($data['priority'])) {
+
+            if (filter_var($data['priority'], FILTER_VALIDATE_INT) == FALSE) {
+                $errors[] = "priority must be an integer";
+            }
+        }
+
+        return $errors;
     }
 }
