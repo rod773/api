f48cc3d3cf09efe8986900ffbae0e090c7001a76
rod773 <104315743+rod773@users.noreply.github.com>
2023-06-30 16:23:09 +0200

    table user

--- api_db.sql
+++ api_db.sql
@@ -1,7 +1,17 @@
-CREATE TABLE api_db.task (
+CREATE TABLE task (
 id INT NOT NULL AUTO_INCREMENT,
 name VARCHAR(128) NOT NULL,
 priority INT DEFAULT NULL,
 is_completed boolean NOT NULL DEFAULT false,
 PRIMARY KEY (id),
 INDEX (name));
+
+CREATE TABLE user (
+id INT NOT NULL AUTO_INCREMENT,
+name VARCHAR(128) NOT NULL,
+username VARCHAR(128) NOT NULL,
+password_hash VARCHAR(255) NOT NULL,
+api_key VARCHAR(32) NOT NULL,
+PRIMARY KEY (id),
+UNIQUE (username),
+UNIQUE (api_key) );
