8d760e4335890416c245a7dd837010d7eff0abca
rod773 <104315743+rod773@users.noreply.github.com>
2023-07-06 14:30:24 +0200

    delete refesh token create new

--- refresh.php
+++ refresh.php
@@ -59,3 +59,10 @@ if ($user === false) {
 }
 
 require "tokens.php";
+
+
+$refresh_token_gateway = new RefreshTokenGateway($database, $_ENV['SECRET_KEY']);
+
+$refresh_token_gateway->delete($data['token']);
+
+$refresh_token_gateway->create($refresh_token, $refresh_tohen_expiry);
--- src/RefreshTokenGateway.php
+++ src/RefreshTokenGateway.php
@@ -15,7 +15,7 @@ class RefreshTokenGateway
 
     public function create($token, $expiry): bool
     {
-        $hash = hash("sha256", $token, $this->key);
+        $hash = hash_hmac("sha256", $token, $this->key);
 
         $sql = "insert into refresh_token (token_hash, expires_at)" .
             "values ( :token_hash, :expires_at)";
@@ -27,5 +27,21 @@ class RefreshTokenGateway
         $stmt->bindValue(":expires_at", $expiry, PDO::PARAM_INT);
 
         return $stmt->execute();
+    }
+
+    public function delete($token): int
+    {
+
+        $hash = hash_hmac("sha256", $token, $this->key);
+
+        $sql = "delete from refresh_token where token_hash = :token_hash";
+
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->bindValue(":token_hash", $hash, PDO::PARAM_STR);
+
+        $stmt->execute();
+
+        return $stmt->rowCount();
     }
 }
